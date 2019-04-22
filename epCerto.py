6564b6e9232de1f2872a18153cc15bd37ae771fd
Joao <johnroxo@hotmail.com>
2019-04-22 17:05:08 -0300

    tp, segunda batalha, falta jadson, luta prof, arrumar dano e recompensas tp

--- epCerto.py
+++ epCerto.py
@@ -14,7 +14,8 @@ def carregar_cenarios():
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
                 "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
+                "biblioteca": "Ir para a biblioteca",
+                "4 andar": "dar uma passada no 4 andar"
             }
         },
         "carona": {
@@ -50,9 +51,40 @@ def carregar_cenarios():
             "descricao": "voce saiu na porrada com o monstro",
             "opcoes": {
                 "saguao insper" : " Ir para entrada do Insper.",
-                "predrio 2": "Tentar fazer o EP com seus amigos"
+                "predio 2": "Tentar fazer o EP com seus amigos"
             }
-        } ,                              
+        } ,
+        "4 andar":{
+            "titulo": "4 andar",
+            "descricao":"Voce subiu no quarto andar para ver como estao as coisas por la",
+            "opcoes":{
+                "jogar":"jogar um pouco para descontrair",
+                "Sala 405": "Dar um alhada na sala para ver se tem algo que pode te ajudar com o EP",
+                "andar professor": "ir para o andar do professor",
+                "saguao insper" : "voltar para o saguo do insper",
+            }
+        },
+        "jogar":{
+            "titulo": "Nada como um bom descanso",
+            "decricao":"Voce descansou bastante e obviamente perdeu o tempo do Ep",
+            "opcoes":{}
+        },
+        "Sala 405":{
+            "titulo": "Ambiente medonho",
+            "descricao": "A sala esta apagada, voce procura algo que pode te ajudar com o EP e acha uma estranha passagem secreta, mas nada e tao facil, voce encontra um monstro que protege a passagem",
+            "opcoes":{
+                "Lutar": "Lutar com o monstro para ver o que tem atras dele.",
+                "fugir": "Sair correndo da sala"
+            }
+        } ,
+        "Lutar":{
+            "titulo":"luta",
+            "descricao": "Voce manda o monstro vir pra cima",
+            "opcoes":{
+                "andar professor": "ir para o andar do professor",
+                "saguao insper" : "voltar para o saguo do insper",
+            }
+        },                        
         "casa":{
             "titulo": "Cidade Raiz",
             "descricao": "Voce esta em Ribeirao Preto (Terra do cafe) em sua casa.",
@@ -97,7 +129,7 @@ def main():
         "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
     print()
     print("Falta 1 dia para entrega do EP!Para sua sorte enquanto estava arrumando suas mala"
-         "encontrou uma gema da vida em um bau antigo  no seu armario. Você está "
+         "encontrou uma gema da vida(ganha +100 de vida) em um bau antigo  no seu armario. Você está "
         "na sua casa, e precisa decidir como vai para Sao Paulo (gema da vida ja esta em seu inventario). "
         )
     print()
@@ -106,10 +138,7 @@ def main():
     gema_EP = False
     game_over = False
     while not game_over:
-        cenario_atual = cenarios[nome_cenario_atual]
-
-        opcoes = cenario_atual['opcoes']
-        
+        cenario_atual = cenarios[nome_cenario_atual]       
         inventario = ["gemaDaVida"]
         print(cenario_atual['titulo'])
         print(len(cenario_atual['titulo'])*'-')
@@ -122,25 +151,68 @@ def main():
 
         monstro = 100       
         gemaDaVida = 100
-        gemaDeAtaque = 20
         jogador = 100
         if 'gemaDaVida' in inventario:
             jogador += gemaDaVida
 #EP feito
         if nome_cenario_atual == 'professor' and gema_EP == True:
              print('parabens, por conta da gema da EP, foi criado um EP perfeito e voce entregou no praso para o professor !!!')                 
-             break
+             break        
 #combate Busao
         if nome_cenario_atual == 'lutar':
             print("E sua primeira batalha, mostre o porque merece adiamento do EP!")
-#combate Busao 
-        if nome_cenario_atual == 'lutar busao':
-
+            print("Voce tem ", jogador, " de vida")
+            while jogador > 0 and monstro > 0:
+                A = int(input("Escolha um numero de 0 a 10: "))
+                B = random.randint(1,10)
+                if A == B:
+                    ataque_player = 20
+                else:
+                    ataque_player = random.randint(15,19)
+                    x = random.randint(1,3)
+                    if x <= 2:
+                        monstro -= ataque_player
+                        print("O ataque que você deu foi efetivo:")
+                        print(ataque_player)
+                        print("a vida do monstro agora é igual a:")
+                        print(monstro)
+                    else:
+                        jogador -= 10
+                        print("O monstro te atingiu, sua vida agora é igual a:")
+                        print(jogador)
+                        vida_jogador = jogador
+            if jogador <=0:
+               print("Você morreu!")
+            else:
+                print("Voce ganhou, parabens!!Sua vida agora é igual a:")
+                print(jogador)
+                print("Após a luta mais acirrada da sua vida, você derrotou o monstro, parabens!!")
+                print('Apos vencer o montro voce chega tranquilamente em SP')
+                y = random.randint(1,100)
+                if y <= 30:
+                    if "gemaDeTeleporte" not in inventario:
+                        inventario.append("gemaDeTeleporte")
+                elif y > 30 and y < 75:
+                    if "gemaDeAtaque" not in inventario:
+                        inventario.append("gemaDeAtaque")
+                elif y > 75 and y <= 99:
+                    print("Porem não ganhou nada, siga a sua jornada!")
+                else:
+                    if "gema de EP feita" not in inventario:
+                        inventario.append("gema de EP feita")
+                        gema_EP = True
+                        print("Parabens!! Voce ganhou a gema de EP concluida, ela torna a sua EP feita e perfeita!Va até o professor e faça a entrega") 
+                print("Este é o seu inventario após a batalha:")
+                print(inventario)
                 
-           print("Você encontrou um monstro, Agora terá que lutar pela sua vida!")
-           print("Voce tem ", jogador, " de vida")
-           print("A luta começou")
-           while jogador > 0 and monstro > 0:
+         
+#Demais combates
+        gemaDeAtaque = 20
+        if 'gemaDeAtaque' in inventario:
+             ataque_player += gemaDeAtaque
+        if nome_cenario_atual == 'Lutar':
+             print("Voce tem ", vida_jogador, " de vida")
+             while vida_jogador > 0 and monstro > 0:
                A = int(input("Escolha um numero de 0 a 10: "))
                B = random.randint(1,10)
                if A == B:
@@ -155,35 +227,44 @@ def main():
                        print("a vida do monstro agora é igual a:")
                        print(monstro)
                    else:
-                        jogador -= 10
-                        print("O monstro te atingiu, sua vida agora é igual a:")
-                        print(jogador)
+                       vida_jogador -= 10
+                       print("O monstro te atingiu, sua vida agora é igual a:")
+                       print(vida_jogador)                      
+             if jogador <=0:
+                 print("Você morreu!")
+             else:
+                 print("Voce ganhou, parabens!!Sua vida agora é igual a:")
+                 print(vida_jogador)
+                 print("Após a luta mais acirrada da sua vida, você derrotou o monstro, parabens!!")          
+                 print("Voce percebeu que ele n escondia so uma passagem e sim um teletransporte")
+                 print("Agora voce precisa escolher entre 3 salas, podendo ganhar itens bons ou ter uma surpresa inesperada")
+                 print("E claro, para se teletransportar voce precisa ter a gema de teleporte no seu inventario.")
+                 
+                 if 'gemaDeTeleporte' in inventario:
+                     sala_secreta = input('qual sala deseja ir(sala 1,sala 2 ou sala 3)??')
+                     if sala_secreta == "sala 1": 
+                         print("Que sorte..., voce ganhou um bonus de vida(+50) por entrar nessa sala.")
+                         vida_jogador += 100
+                         print("Agora voce tem" , vida_jogador, "de vida")
+                     elif sala_secreta == "sala 2": 
+                         print("Que sorte..., voce ganhou uma gema de ataque(+20) por entrar nessa sala.")
+                         if "gemaDeAtaque" not in inventario:
+                             inventario.append("gemaDeAtaque")
+                     else:
+                         print("Que pena.., voce perdeu 30 de vida por entrar nessa sala.")
+                         vida_jogador -= 30
+                         print("Agora voce tem" , vida_jogador, "de vida")
+                 else:
+                     print("infelizmente voce nao possui a gema de teleporte e seu inventario")
         
-           if jogador <=0:
-               print("Você morreu!")
-           else:
-               print("Voce ganhou, parabens!!Sua vida agora é igual a:")
-               print(jogador)
-               print("Após a luta mais acirrada da sua vida, você derrotou o monstro, parabens!!")
-               y = random.randint(1,100)
-           if y <= 30:
-               if "gema de teleporte" not in inventario:
-                   inventario.append("gema de teleporte")
-           elif y > 30 and y < 75:
-               if "gemaDeAtaque" not in inventario:
-                   inventario.append("gemaDeAtaque")
-           elif y > 75 and y <= 99:
-               print("Porem não ganhou nada, siga a sua jornada!")
-           else:
-               if "gema de EP feita" not in inventario:
-                   inventario.append("gema de EP feita")
-                   gema_EP = True
-                   print("Parabens!! Voce ganhou a gema de EP concluida, ela torna a sua EP feita e perfeita!Va até o professor e faça a entrega")
-           print("Este é o seu inventario após a batalha:")
-           print(inventario)
-         
-#Demais combates
-       
+        if nome_cenario_atual == "professor":
+            professor = 500
+            
+            
+        
+            
+                 
+        
            
        
                         
@@ -211,8 +292,10 @@ def main():
     
         
            
-
-    print("Você perdeu!")
+    if "gema de EP feita" in inventario:
+        print("parabens, voce ganhou gracas a gema da ep feita!!!")
+    else:
+        print("Você perdeu!")
 
 
 # Programa principal.
